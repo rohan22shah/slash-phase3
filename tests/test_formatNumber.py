cf3dc2533eea273bea7eef3ffe786ae42a8b696d
Rohan Shah <42119767+rohan22shah@users.noreply.github.com>
2021-12-04 02:20:38 +0530

    Update test_formatNumber.py

--- tests/test_formatNumber.py
+++ tests/test_formatNumber.py
@@ -12,7 +12,7 @@ import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-import src.scraper.formattr as formatter
+import src.formattr as formatter
 import math
 
 def test_getNumbers():
