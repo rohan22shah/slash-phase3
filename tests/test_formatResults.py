b20609c84e179d1f4b0f510d5e9bf727c69c2cde
Rohan Shah <42119767+rohan22shah@users.noreply.github.com>
2021-12-04 02:21:06 +0530

    Update test_formatResults.py

--- tests/test_formatResults.py
+++ tests/test_formatResults.py
@@ -13,7 +13,7 @@ import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-import src.scraper.formattr as formatter
+import src.formattr as formatter
 from bs4 import BeautifulSoup
 
 def test_sortList():
