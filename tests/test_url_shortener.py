740f308e6271bac9c0e013a6494f76aa93e0d3d2
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-12-03 17:02:56 -0500

    Update test_url_shortener.py

--- tests/test_url_shortener.py
+++ tests/test_url_shortener.py
@@ -1,3 +1,5 @@
+from src.scraper.url_shortener import shorten_url
+
 def test_url_shortener():
     url = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
     s_url = shorten_url(url)
