0237fc67d1d88f84a132e53979d0489d0f5983df
Anshul Patel <36321866+anshulp2912@users.noreply.github.com>
2021-12-03 00:41:38 -0500

    Added files for test cases

---
+++ tests/test_url_shortener.py
@@ -0,0 +1,4 @@
+def test_url_shortener():
+    url = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
+    s_url = shorten_url(url)
+    assert 'tinyurl.com' in s_url
