4b0e452e4eb13ec7e876f672cdaa8c8d75ce690a
Rohan Shah <42119767+rohan22shah@users.noreply.github.com>
2021-12-04 02:21:25 +0530

    Update test_formatText.py

--- tests/test_formatText.py
+++ tests/test_formatText.py
@@ -13,7 +13,7 @@ import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-import src.scraper.formattr as formatter
+import src.formattr as formatter
 
 def test_formatSearchQuery():
     """
