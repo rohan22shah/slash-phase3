baa061008356124b0b2d6a0facda742aeb8c06ee
Anshul Patel <36321866+anshulp2912@users.noreply.github.com>
2021-12-03 15:01:40 -0500

    Update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,47 @@
-"""
-Copyright (C) 2021 SE Slash - All Rights Reserved
-You may use, distribute and modify this code under the
-terms of the MIT license.
-You should have received a copy of the MIT license with
-this file. If not, please write to: secheaper@gmail.com
+from setuptools import setup
 
-"""
-
-from setuptools import setup, find_packages
-
-setup(
-    name='Slash',
-    version='1.0.0',
-    install_requires=[
-        'pytest'
-    ],
-    packages=find_packages()
-)
+setup(name='slash',
+      version='3.0',
+      description='Slash is a command line tool that scrapes the most popular e-commerce websites to get the best deals on the searched items across these websites.',
+      author='Anshul, Bhavya, Darshan, Pragna, Rohan',
+      author_email='rohan22shah@gmail.com',
+      url='https://github.com/rohan22shah/slash-phase3.git',
+      packages=['slash'],
+      long_description="""\
+            Hands on for the standard github repo files.
+            .gitignore
+            .travis.yml
+            CITATION.md : fill on once you've got your ZENODO DOI going
+            CODE-OF-CONDUCT.md
+            CONTRIBUTING.md
+            LICENSE.md
+            README.md
+            setup.py
+            requirements.txt
+            test/
+              README.md
+            src/
+              __init__.py
+        """,
+      classifiers=[
+            "License :: MIT License",
+            "Programming Language :: Python",
+            "Development Status :: Initial",
+            "Intended Audience :: Developers",
+            "Topic :: Software Engineering",
+        ],
+      keywords='python requirements license gitignore',
+      license='MIT',
+      install_requires=[
+            'BeautifulSoup',
+            'pytest',
+            'uvicorn',
+            'streamlit',
+            'webdriver_manager',
+            'pyshorteners',
+            'link-button',
+            'ebaysdk',
+            'requests',
+            'pandas'
+        ],
+     )
