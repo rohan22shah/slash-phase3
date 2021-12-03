679b2d470b23bb49df481327a8d5ef47491995b6
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-12-03 16:06:01 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,12 @@ Created on Tue Nov 30 00:03:30 2021
 
 @author: Rohan Shah
 """
-
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 from src.main_streamlit import search_items_API
     
 def test_api_amazon():
