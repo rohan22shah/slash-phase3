7c9b1abc1581cadcc7a7a9aa7f45c7af1ed2fe9e
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-12-04 12:08:10 -0500

    Updated test files

---
+++ tests/test_api_ebay.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 00:03:30 2021
+
+@author: Rohan Shah
+"""
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+from src.main_streamlit import search_items_API
+
+def test_api_ebay():
+    product = 'lenovo'
+    site = 'eb'
+    result = search_items_API(site, product)
+    assert result is not None
